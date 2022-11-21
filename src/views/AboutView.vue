89bbce99a5f854f3b95ffba487ee29210fa43ff2
unknown <timafra2@mallon.com.br>
2022-11-21 10:49:48 -0300

    add files

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <TheHeader/>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader.vue'
+
+    export default {
+      name: 'AboutView',
+      components: {
+        TheHeader
+      }
+    }  
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <TheHeader v-if="showHeader"></TheHeader>
+  <div v-if="showNames">
+    Nome: {{ name }}
+    <br>
+    Sobrenome: {{ lastname }}
+  </div><br>
+  nivel: 
+  <div v-if="nivel === 'admin'">Admin</div>
+  <div v-else-if="nivel === 'gerente'">Gerente</div>
+  <div v-else >Usuário</div>
+
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import TheHeader from '@/components/TheHeader.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+    TheHeader
+  },
+  data() { //variaveis do componente
+    return{
+      showHeader: true,
+      name: 'Luccas',
+      lastname: 'Drapala',
+      showNames: false,
+      nivel: 'erro'
+    }
+  }
+}
+</script>
