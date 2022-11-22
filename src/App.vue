c6f69b29bb64d532ee4ba414b823a05148e2d34c
unknown <timafra2@mallon.com.br>
2022-11-22 17:53:36 -0300

    add files to-do-list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+
+<body class="bg-gray-700">
+    <!-- Content -->
+    <div class="px-3 py-10 md:px-10">
+        <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+
+            <TodoSpinner v-if="loading" />
+
+            <div v-else>
+                <TodoFormAdd/>
+
+                <TodoItems/>
+
+                <TodoEmpty/>
+            </div>
+        </div>
+    </div>
+    <!--/ Content -->
+
+</body>
+</html>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoSpinner from './components/TodoSpinner.vue';
+import TodoFormAdd from './components/TodoFormAdd.vue';
+import TodoItems from './components/TodoItems.vue';
+import TodoEmpty from './components/TodoEmpty.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    components: 
+    { TodoSpinner, TodoFormAdd, TodoItems, TodoEmpty },
+
+    data() {
+        return {
+            loading: false
+        }
+    },
+
+    created(){
+        this.loading = true
+        this.$store.dispatch('getTodos').finally(() => {
+            this.loading = false
+        })
+    },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/store/index.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { createStore } from 'vuex'
+
+
+export default createStore({
+  state: {
+    todos: []
+  },
+  mutations: {
+    storeTodos(state, payload) {
+      state.todos = payload
+    },
+    storeTodo(state, payload) {
+      state.todos.push(payload)
+    }
+  },
+  actions: {
+    getTodos({commit}){
+      return axios.get('http://localhost:3000/todos')
+      .then((response) =>{
+        commit('storeTodos', response.data)
+      })
+    },
+    addTodo({commit}, data) {
+      axios.post('http://localhost:3000/todos', data).then((response) => {
+        commit('storeTodo', response.data)
+      })
+    },
+  },
+  getters: {
+  },
+  modules: {
+  }
+})
