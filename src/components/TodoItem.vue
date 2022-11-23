f302a3219f6d0168b4458521451c42d0e6ad748a
unknown <timafra2@mallon.com.br>
2022-11-23 09:57:19 -0300

    configurando botao de completed e checked

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,21 +3,28 @@
     <div class="bg-gray-300 rounded-sm">
         <div class="flex items-center px-4 py-3 border-b border-gray-400 last:border-b-0">
             <div class="flex items-center justify-center mr-2">
-                <button class="text-gray-400">
-                <svg class="w-5 h-5" fill="none" 
-                    stroke="currentColor" viewBox="0 0 24 24" 
-                    xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" 
-                    stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>
+                <button 
+                    :class="{
+                        'text-gray-400': !isCompleted,
+                        'text-green-600': isCompleted,
+                    }"
+                    @click="onCheckClick"
+                    >
+                    <svg class="w-5 h-5" fill="none" 
+                        stroke="currentColor" viewBox="0 0 24 24" 
+                        xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" 
+                        stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                    </svg>
                 </button>
             </div>
 
             <div class="w-full">
                 <input
+                    v-model="title"
                     type="text"
                     placeholder="Digite a sua tarefa"
-                    :value="todo.title"
                     class="bg-gray-300 placeholder-gray-500 text-gray-700 font-light focus:outline-none block w-full appearance-none leading-normal mr-3"
-                    @keyup.enter="updateTodo"
+                    @keyup.enter="onTitleChange"
                 >
             </div>
 
@@ -55,23 +62,38 @@ export default{
           default: () =>  ({})
         }
     },
+
+    data() {
+        return {
+            title: this.todo.title,
+            isCompleted: this.todo.completed
+        }
+    },
+
     methods: {
-        updateTodo($evt) {
+        onTitleChange() {
 
-            const newTitle = $evt.target.value
-
-            if(!newTitle){
-                return false;
+            if(!this.title){
+                return false
             }
 
+            this.updateTodo()
+        },
+
+        updateTodo() {
             const data = {
                 id: this.todo.id,
                 data: {
-                    newTitle,
-                    completed: this.todo.completed
+                    title: this.title,
+                    completed: this.isCompleted
                 }
             }
             this.$store.dispatch('updateTodo', data)
+        },
+
+        onCheckClick() {
+            this.isCompleted = !this.isCompleted
+            this.updateTodo()
         }
     },
 }
